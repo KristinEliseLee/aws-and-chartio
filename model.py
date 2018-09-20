35cd88c86e5b8344bc20ade830f7c88095c2ba22
Kristin Lee <serpent.kris@gmail.com>
2018-09-20 16:34:39 -0700

    basic working model and server

---
+++ model.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+
+db = SQLAlchemy()
+
+def connect_to_db(app, db_name):
+    """Connect to the db"""
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://{}:{}@{}/{}'.format(os.environ[
+    'DB_USERNAME'], os.environ['DB_PASSWORD'],os.environ['DB_SERVER'], db_name)
+    app.config['SQLALCHEMY_ECHO'] = False
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+
+
+class Team(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+    date_created = db.Column(db.DateTime, server_default=db.func.now())
+    date_updated = db.Column(db.DateTime, server_default=db.func.now(),
+        server_onupdate=db.func.now())
+    users = db.relationship('User', backref='team')
+    total_points = db.relationship('Point', secondary='user')
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+    team_id = db.Column(db.Integer, db.ForeignKey('team.id'), nullable=False)
+    date_created = db.Column(db.DateTime, server_default=db.func.now())
+    date_updated = db.Column(db.DateTime, server_default=db.func.now(),
+        server_onupdate=db.func.now())
+
+class Point(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    reason = db.Column(db.Text)
+    date_created = db.Column(db.DateTime, server_default=db.func.now())
+
+if __name__ == '__main__':
+    from server import app
+    connect_to_db(app, 'teampoints')
+    db.create_all()
---
+++ server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from model import connect_to_db, User, Team, Point, db
+
+app=Flask(__name__)
+connect_to_db(app, 'teampoints')
+
+@app.route('/')
+def show_points():
+    teams = Team.query.all()
+    return render_template('showpoints.html', teams=teams)
+
+
+if __name__ == '__main__':
+
+    app.debug = False
+
+
+    # Use the DebugToolbar
+    # DebugToolbarExtension(app)
+
+    app.run(host="0.0.0.0")
